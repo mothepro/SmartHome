f62fe2c0231f5fc351d327a5ca54d33176da72d2
Maurice Prosper <maurice.prosper@ttu.edu>
2016-03-10 14:52:15 -0600

    Removed unused classes Improved example to show all devices

--- src/Example.java
+++ src/Example.java
@@ -1,22 +1,25 @@
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 /**
  * Simple Serial Example
  */
-public class Test implements SerialListener {
+public class Example implements SerialListener {
     Serial serial;
 
     /**
      * Connect to a device by name
      * @param names
      */
-    public Test(String[] names) {
+    public Example(String[] names) {
         for (String s: names) {
             System.out.println("Attempting to connect to device: " + s);
 
             try {
                 serial = new Serial(s);
+                System.out.println("Success!");
+
                 serial.addListener(this);
                 send("Hello");
             } catch (Exception e) {
@@ -32,13 +35,21 @@ public class Test implements SerialListener {
     public static void main(String[] args) {
         // without arguments
         if(args.length == 0) {
-            System.out.println("Enter a Device Port Name to connect to:");
+            List<String> devs = Serial.getPortNames();
+
+            System.out.println("We found "+ devs.size() +" devices.");
+            for (String s : devs)
+                System.out.println("- "+ s);
+
+            System.out.println();
+            System.out.println("Enter a Device Port Name to connect to");
 
             Scanner reader = new Scanner(System.in);
-            args[0] = reader.nextLine();
+
+            args = new String[] { reader.nextLine() };
         }
 
-        new Test(args);
+        new Example(args);
 
         //the following lines will keep this app alive for 1000 seconds,
         new Thread() {
